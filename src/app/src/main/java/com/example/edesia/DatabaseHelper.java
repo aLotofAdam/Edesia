6ea5737ebb94f059062e5bc565f8ce3251b3ac0d
stephenbennett2 <scbenne3@uncg.edu>
2019-03-24 12:49:35 -0400

    finishing up xml edits for login and sign_up, starting connections from LoginActivity and SignUpActivity

--- src/app/src/main/java/com/example/edesia/DatabaseHelper.java
+++ src/app/src/main/java/com/example/edesia/DatabaseHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
 
 
 public class DatabaseHelper {
@@ -22,11 +23,13 @@ public class DatabaseHelper {
         private static final String TABLE_NAME = "UserTable";
         private static final int DATABASE_Version = 1;
         private static final String USER_ID = "ID";
+        private static final String USER_NAME = "Username";
         private static final String NAME = "Name";
+        private static final String EMAIL = "Email";
         private static final String PASSWORD = "Password";
-        private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME +
-                                    " ("+USER_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                                    ""+NAME+" VARCHAR(255) ,"+ PASSWORD +" VARCHAR(225));";
+        private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " ("+USER_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                    +USER_NAME+" VARCHAR(255),"+EMAIL+" VARCHAR(255),"
+                                    +NAME+" VARCHAR(255),"+ PASSWORD +" VARCHAR(225));";
         private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
         private Context context;
 
@@ -53,11 +56,14 @@ public class DatabaseHelper {
             }
         }
     }
-    public long insertData(String name, String pass) {
+    public long insertData(String name, String username, String email, String pass, String confirmPass) throws Exception{
 
         SQLiteDatabase dbb = DbHelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(dbHelper.NAME, name);
+        contentValues.put(dbHelper.USER_NAME, username);
+        contentValues.put(dbHelper.EMAIL, email);
+
         contentValues.put(dbHelper.PASSWORD, pass);
         long id = dbb.insert(dbHelper.TABLE_NAME, null , contentValues);
         return id;
@@ -66,35 +72,37 @@ public class DatabaseHelper {
     public String getData() {
 
         SQLiteDatabase db = DbHelper.getWritableDatabase();
-        String[] columns = {dbHelper.USER_ID, dbHelper.NAME, dbHelper.PASSWORD};
+        String[] columns = {dbHelper.USER_ID, dbHelper.NAME, dbHelper.USER_NAME, dbHelper.EMAIL, dbHelper.PASSWORD};
         Cursor cursor = db.query(dbHelper.TABLE_NAME,columns,null,null,null,null,null);
         StringBuffer buffer= new StringBuffer();
         while (cursor.moveToNext())
         {
-            int cid = cursor.getInt(cursor.getColumnIndex(dbHelper.USER_ID));
+            int uid = cursor.getInt(cursor.getColumnIndex(dbHelper.USER_ID));
             String name =cursor.getString(cursor.getColumnIndex(dbHelper.NAME));
+            String username =cursor.getString(cursor.getColumnIndex(dbHelper.USER_NAME));
+            String email =cursor.getString(cursor.getColumnIndex(dbHelper.EMAIL));
             String  password =cursor.getString(cursor.getColumnIndex(dbHelper.PASSWORD));
-            buffer.append(cid+ "   " + name + "   " + password +" \n");
+            buffer.append(uid+ "   " + name + "   " + username + "   " + email + "   " + password +" \n");
         }
         return buffer.toString();
     }
 
-    public int updateName(String old , String newName) {
+    public int updateUsername(String old , String newName) {
 
         SQLiteDatabase db = DbHelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(dbHelper.NAME,newName);
-        String[] whereArgs= {old};
-        int count =db.update(dbHelper.TABLE_NAME,contentValues, dbHelper.NAME+" = ?",whereArgs );
+        contentValues.put(dbHelper.USER_NAME,newName);
+        String[] findUser= {old};
+        int count =db.update(dbHelper.TABLE_NAME,contentValues, dbHelper.USER_NAME+" = ?",findUser );
         return count;
     }
 
     public  int delete(String User) {
 
         SQLiteDatabase db = DbHelper.getWritableDatabase();
-        String[] find ={User};
+        String[] findUser ={User};
 
-        int count =db.delete(dbHelper.TABLE_NAME , dbHelper.NAME+" = ?",find);
+        int count =db.delete(dbHelper.TABLE_NAME , dbHelper.USER_NAME+" = ?",findUser);
         return  count;
     }
 }
--- src/app/src/main/java/com/example/edesia/LoginActivity.java
+++ src/app/src/main/java/com/example/edesia/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.example.edesia;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    private TextInputEditText textInputUsername;
+    private TextInputEditText textInputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
