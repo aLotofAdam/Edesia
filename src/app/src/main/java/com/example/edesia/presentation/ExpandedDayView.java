e92f42fe216a517e3d9e4a52839fca7478c3ae2f
mccauley747 <jbholley@uncg.edu>
2019-04-04 18:17:42 -0400

    Updated GUI and Java functions

--- src/app/src/main/java/com/example/edesia/presentation/ExpandedDayView.java
+++ src/app/src/main/java/com/example/edesia/presentation/ExpandedDayView.java
@@ -1,134 +1,72 @@
 package com.example.edesia.presentation;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ExpandedDayView.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ExpandedDayView#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ExpandedDayView extends Fragment implements AdapterView.OnItemSelectedListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public ExpandedDayView() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ExpandedDayView.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ExpandedDayView newInstance(String param1, String param2) {
-        ExpandedDayView fragment = new ExpandedDayView();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class ExpandedDayView extends AppCompatActivity{
+    /*
+    Button b4 = findViewById(R.id.meal_randomizer2);
+    Button b5 = findViewById(R.id.search2);
+    Button b6 = findViewById(R.id.grocery_list2);
+    Button b7 = findViewById(R.id.back_to_home);
+*/
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        setContentView(R.layout.content_expanded_day_view);
+/*
+        b4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openMealRandomizer();
+            }
+        });
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-    //    textView.setText(R.string.hello_blank_fragment);
-        return textView;
-    }
+        b5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openRecipeSearch();
+            }
+        });
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        b6.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openGroceryList();
+            }
+        });
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        b7.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openHome();
+            }
+        });
+*/
     }
 /*
-    @Override
-    public void onCreate(Bundle savedInstancedState){
-        super.onCreate(savedInstancedState);
-        setContentView(R.layout.content_expanded_day_view);
+    public void openMealRandomizer(){
+        Intent intent = new Intent(this, MealRandomizer.class);
+        startActivity(intent);
+    }
 
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this, R.array.Months, android.R.layout.simple_spimmer_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item-);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+    public void openRecipeSearch(){
+        Intent intent = new Intent(this, RecipeSearch.class);
+        startActivity(intent);
+    }
+
+    public void openGroceryList(){
+        Intent intent = new Intent(this, GroceryList.class);
+        startActivity(intent);
+    }
+
+    public void openHome(){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 */
 }
